3dd15cb314145fff2e11b3b181292df6e7b15049
Dark <56140684+Evandro9820@users.noreply.github.com>
2023-11-13 20:00:29 -0300

    Correto

--- Programação Web/JAVASCRIPT/Aula 6 - Gameficação/script.js
+++ Programação Web/JAVASCRIPT/Aula 6 - Gameficação/script.js
@@ -37,3 +37,35 @@ function limparFormulario() {
 
 }
 
+function proximaEtapa() {
+  moverEtapa(1);
+}
+
+function etapaAnterior() {
+  moverEtapa(-1);
+}
+
+function moverEtapa(direcao) {
+  var elementoEtapaAtual = document.querySelector('.pagina-formulario:not([style="display: none;"])');
+  if (!elementoEtapaAtual) {
+      document.getElementById('pagina-1').style.display = 'block';
+      return;
+  }
+  
+  var numeroEtapaAtual = parseInt(elementoEtapaAtual.getAttribute('data-etapa'));
+  elementoEtapaAtual.style.display = 'none';
+  
+  var numeroProximaEtapa = numeroEtapaAtual + direcao;
+  var elementoProximaEtapa = document.getElementById('pagina-' + numeroProximaEtapa);
+  
+  if (elementoProximaEtapa) {
+      elementoProximaEtapa.style.display = 'block';
+  } else {
+      elementoEtapaAtual.style.display = 'block';
+      if (direcao > 0) {
+          alert("Você completou o formulário!");
+      }
+  }
+}
+
+
